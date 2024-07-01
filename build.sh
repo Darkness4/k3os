86322d9916731221551a4b82786457495136377d
Nguyen Marc <nguyen_marc@live.fr>
2024-07-01 03:23:24 +0200

    AIO build script

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -ex
+
 sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # Remove unix:// from DOCKER_HOST
 DOCKER_HOST=$(echo ${DOCKER_HOST} | sed 's/^unix:\/\///')
+
+GIT_TAG=$(git describe --tags --abbrev=0)
 
 rm -rf ./build/
 
@@ -15,6 +19,13 @@ podman run --rm -it \
   -e ARCH=amd64 \
   -v "${DOCKER_HOST}:/var/run/docker.sock" \
   ghcr.io/darkness4/k3os:latest-base
+
+podman tag ghcr.io/darkness4/k3os:latest "ghcr.io/darkness4/k3os:latest-amd64"
+podman tag ghcr.io/darkness4/k3os:latest "ghcr.io/darkness4/k3os:${GIT_TAG}-amd64"
+podman push "ghcr.io/darkness4/k3os:${GIT_TAG}-amd64"
+podman push "ghcr.io/darkness4/k3os:latest-amd64"
+# shellcheck disable=SC2046
+podman rmi -f $(podman images --filter=reference="ghcr.io/darkness4/k3os*:dev" -q)
 
 rm -rf ./build/
 
@@ -29,3 +40,15 @@ podman run --rm -it \
   -e GOARCH=arm64 \
   -v "${DOCKER_HOST}:/var/run/docker.sock" \
   ghcr.io/darkness4/k3os:latest-base
+
+podman tag ghcr.io/darkness4/k3os:latest ghcr.io/darkness4/k3os:latest-arm64
+podman tag ghcr.io/darkness4/k3os:latest "ghcr.io/darkness4/k3os:${GIT_TAG}-arm64"
+podman push "ghcr.io/darkness4/k3os:${GIT_TAG}-arm64"
+podman push "ghcr.io/darkness4/k3os:latest-arm64"
+
+# Build the manifest
+podman manifest create ghcr.io/darkness4/k3os:${GIT_TAG} \
+  ghcr.io/darkness4/k3os:${GIT_TAG}-amd64 \
+  ghcr.io/darkness4/k3os:${GIT_TAG}-arm64
+
+podman manifest push --all ghcr.io/darkness4/k3os:${GIT_TAG} docker://ghcr.io/darkness4/k3os:${GIT_TAG}
